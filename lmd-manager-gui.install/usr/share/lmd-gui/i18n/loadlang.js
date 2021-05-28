e292b09482759dcae4f1fb35b42bc702929bc56e
Raul Rodrigo Segura <raurodse@gmail.com>
2021-05-28 17:37:58 +0200

    remove old project

--- lmd-manager-gui.install/usr/share/lmd-gui/i18n/loadlang.js
+++
@@ -1,9 +0,0 @@
-var language = process.env.LANG;
-var i18n = new I18n(language,'lmd-manager');
-try{
-	i18n.loadfile();
-}
-catch(err){
-	var i18n = new I18n('en','lmd-manager');
-	i18n.loadfile();
-}
