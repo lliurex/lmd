89a9cfaf71617f5a4d6a9e9d39f306bf3005e695
Raul Rodrigo Segura <raurodse@gmail.com>
2021-06-04 17:16:02 +0200

    migragion in progress

--- lmd-server.install/usr/share/n4d/python-plugins/LmdImageManager.py
+++ lmd-server.install/usr/share/n4d/python-plugins/LmdImageManager.py
@@ -1,353 +1,222 @@
 import json
-import os
-import subprocess
+import sh
 
+from shutil import rmtree
+from sh import mount, umount, lliurex_version
+from pathlib import Path
+
+import n4d.responses
+from n4d.server import Core
 
 class LmdImageManager:
-	
-		
-	def __init__(self):
-		self.imagepath="/etc/ltsp/images/"
-		self.tftppath="/var/lib/tftpboot/ltsp"
-		
-		pass
-	#def __init__
-	
-	def getImageList(self):
-		'''
-		Reads the file list of templates from /etc/ltsp/images
-		Returna a JSON List.
-		'''
-		# 1. /opt/ltsp/name-chroot
-		# 2. /opt/ltsp/images/name-chroot.img
-		# 3. /var/lib/tftpboot/ltsp/name-chroot
-		# if 1,2 and 3 exist -> show
-		# if 1 but not exist 2 or/and 3 -> show with error
-		#
+    
 
-		imagelist=[]
-		
-		for i in os.listdir(self.imagepath):
-			if i.endswith('.json'): 
-				imagelist.append(str(i))
-				
-		return json.dumps(imagelist)
-			
-			
-	# END def GetListImages(self)
+    GET_IMAGE_ERROR = -50
+    MIRROR_NOT_EXISTS = -40
 
-	def getImage(self, image):
-		'''
-		Returns the metadata from certain image
-		
-		// WIP HERE
-		
-		-> mirar si accepta be el tercer parametre o cal fer el kwargs...
-		agafa aquest taskid i comprova amb getTask si existeix i el seu estat
-		i l'afig amb data["taskstatus"]=ret(....)
-		
-		Aixi al renderImage de l'ImageManager ja podem tindre disponible l'estat sense haver
-		d'estar consultant-ho des de la gui
-		'''
-		
-		try:
-			json_data=open(self.imagepath+image)
-			data = json.load(json_data)
-			json_data.close()
-			
-			if "taskid" in data:
-				taskid=data["taskid"];
-			else:
-				taskid="0";
-			
-			# Searching task id for this image
-			status="DONE";
-			ret=objects['TaskMan'].getTaskStatus(taskid);
-			
-			if ret["status"]==True:
-				status=ret["taskStatus"]
-			# if ret[status] is false the task has been done time ago
-			data["task_status"]=status;
-			
-			xpraConnections=objects["RemoteWebGui"].getXpraConnections(image.replace(".json", ""));
-			data["xpraConnections"]=xpraConnections;
-			
-			
-			return json.dumps(data)
-			#return data;
-		except Exception as e:
-			print("[LmdImageManager]: getImage Exception "+str(e))
-			return str(e);
+    def __init__(self):
+        self.configimagepath = Path("/etc/ltsp/images/")
+        self.tftppath = Path("/var/lib/tftpboot/ltsp")
+        self.chrootpath = Path("/opt/ltsp/")
+        self.imagepath = Path("/opt/ltsp/images/")
+        self.lmd_editing_path = Path("/tmp/.lmd-editing-chroot")
 
-		# END def getListTemplate(self, image)
-		
-		
-	def setImage(self, image, data):
-		'''
-		Saves metadata from *data to image
-		data is unicoded string
-		image is name
-		'''
-				
-		path_to_write = os.path.join(self.imagepath,image + ".json")
-		f = open(path_to_write,'w')
-		f.writelines(data)
-		f.close()
+        self.core = Core.get_core() 
+    #def __init__
+        
+    def getImageList(self):
+        '''
+        Reads the file list of templates from /etc/ltsp/images
+        Returna a JSON List.
+        '''
+        # 1. /opt/ltsp/name-chroot
+        # 2. /opt/ltsp/images/name-chroot.img
+        # 3. /var/lib/tftpboot/ltsp/name-chroot
+        # if 1,2 and 3 exist -> show
+        # if 1 but not exist 2 or/and 3 -> show with error
+        #
 
-		try:
-			'''
-			Writing lts.conf for image
-			'''
-			
-			'''ldm_session default
-			ltsp_fatclient undefined
-			fat_ram_threshold default
-			lmd_extra_params'''
+        return n4d.responses.build_successful_call_response( json.dump(i.name for i in self.configimagepath.glob("**/*.json")]) )
+                               
+                    
+        # END def GetListImages(self)
 
-			# Checking if we need to write file
-			
-			jsondata=json.loads(data)
-			
-			if ((jsondata['ldm_session']!="default") or (jsondata['ltsp_fatclient']!="undefined") or (jsondata['fat_ram_threshold']!="default") or (jsondata['lmd_extra_params']!="") or (jsondata['ldm_language']!="default")):
-				tftpfilename=os.path.join(self.tftppath,image + "/lts.conf")
-				
-				f=open(tftpfilename,'w')
-				f.write("[Default]\n")
-				if (jsondata['ldm_session']!="default"):
-					f.write("LDM_SESSION="+jsondata['ldm_session']+"\n")
-				if (jsondata['ldm_language']!="default"):
-					f.write("LDM_LANGUAGE="+jsondata['ldm_language']+"\n")
-				if (jsondata['ltsp_fatclient']!="undefined"):
-					f.write("LTSP_FATCLIENT="+jsondata['ltsp_fatclient']+"\n")
-				if (jsondata['fat_ram_threshold']!="default"):
-					f.write("FAT_RAM_THRESHOLD="+jsondata['fat_ram_threshold']+"\n")
-				if (jsondata['ltsp_fatclient']=="false"):
-					f.write('DEFAULT_DISPLAY_MANAGER="/usr/sbin/ldm"\n')
-					f.write('X_SMART_COLOR_DEPTH=false\n')
-				if (jsondata['use_local_apps']!="false"):
-					f.write("LOCAL_APPS_EXTRAMOUNTS=/home\n")
-					f.write("LOCAL_APPS=true\n")
-					f.write("LOCAL_APPS_MENU=true\n")
-					f.write("LOCAL_APPS_MENU_ITEMS="+jsondata['local_apps_text']+"\n")
+    def getImage(self, image):
+        '''
+        Returns the metadata from certain image
+        
+        // WIP HERE
+        
+        -> mirar si accepta be el tercer parametre o cal fer el kwargs...
+        agafa aquest taskid i comprova amb getTask si existeix i el seu estat
+        i l'afig amb data["taskstatus"]=ret(....)
+        
+        Aixi al renderImage de l'ImageManager ja podem tindre disponible l'estat sense haver
+        d'estar consultant-ho des de la gui
+        '''
+        
+        task_manager = self.core.get_plugin("TaskMan")
+        remote_web_gui = self.core.get_plugin("RemoteWebGui")
 
-				if (jsondata['lmd_extra_params']!=""):
-					extra_params=jsondata['lmd_extra_params'].split("<br/>")
-					for param in extra_params:
-						# Checking syntax and writing
-						if("=" not in param): continue
-						else:
-							f.write(param+"\n")
-				f.close()
-			else:				
-				# We need to check if there was an old config...
-				tftpfilename=os.path.join(self.tftppath,image + "/lts.conf")				
-				try:
-					os.remove(tftpfilename)
-				except:
-					pass
-				
-			return {"status":True, "msg":"Config Done"}
-		
-		except Exception as e:
-			return {"status":False, "msg":str(e)}
-			
-			
-		'''if(data.)
-		path_to_write = os.path.join(self.imagepath,image + ".json")
-		f = open(path_to_write,'w')
-		f.writelines(data)
-		f.close()'''
+        try:
+
+            with self.configimagepath.joinpath(image).open('r') as fd :
+                data = json.load(fd)
+            
+            if "taskid" in data.keys():
+                taskid = data["taskid"]
+            else:
+                taskid = "0"
+
+            # Searching task id for this image
+            ret = task_manager.getTaskStatus(taskid)
+                
+            data["task_status"] = "DONE"
+            if ret["status"] == True :
+                data["task_status"] = ret["taskStatus"]
+
+            '''DEPRECATED 31/5/21
+            xpraConnections = remote_web_gui.getXpraConnections(image.replace(".json", ""));
+            data["xpraConnections"]=xpraConnections;
+            '''
+                
+            return n4d.responses.build_successful_call_response(json.dumps(data))
+
+        except Exception as e:
+            return n4d.responses.build_failed_call_response(LmdImageManager.GET_IMAGE_ERROR)
 
 
-		'''
-		datafile="";
-		for i in data:
-			print i
-			print "*****"
-			datafile=datafile+i+" ";
-				
-		jsondata=json.loads(datafile)
-		print type(data)
-		print "****************"
-		print type(datafile)
-		
-		fd=open(self.imagepath+image, 'w')
-		fd.write('{"id":"'+jsondata['id']+'",\n')
-		fd.write('"name":"'+jsondata['name']+'",\n')
-		fd.write('"template":"'+jsondata['template']+'",\n')
-		fd.write('"desc":"'+(jsondata['desc']).encode('utf8')+'",\n')
-		fd.write('"img":"'+jsondata['img']+'"}\n')
-		fd.close()
-		'''
-	# def setImage(self, image, data)
-	
-	
-	def setStatusImage(self, img_id, status):
-		
-		json_data=open(self.imagepath+img_id+".json")
-		data = json.load(json_data)
-		json_data.close()
-		
-		# Set status
-		data["status"]=status
-		
-		self.setImage(img_id, json.dumps(data));
-		
-	
-	def setNewTaskIdForImage(self, img_id, newid):
-		
-		json_data=open(self.imagepath+img_id+".json")
-		data = json.load(json_data)
-		json_data.close()
-		
-		# Set status
-		data["taskid"]=newid
-		
-		self.setImage(img_id, json.dumps(data));
-		
-		
-	
-	def deleteImage(self, img_id):
-		'''
-		N4d Method to delete an image identified by img_id
-		'''
-		import shutil;
-		
-		try:
-			chroot="/opt/ltsp/"+str(img_id)
-			image="/opt/ltsp/images/"+str(img_id)+".img"
-			json_file="/etc/ltsp/images/"+str(img_id)+".json"
-			tftpboot="/var/lib/tftpboot/ltsp/"+str(img_id)
-			nbd="/etc/nbd-server/conf.d/ltsp_"+str(img_id)+".conf"
-			
-			# Umount anything mounted under image
-			test_chroot=self.umount_chroot(chroot);
-			if test_chroot['status']==False:
-				return test_chroot;
-			
-			# Remove chroot
-			if (os.path.isdir(chroot)):
-				shutil.rmtree(chroot);
-			
-			# Removing .img
-			if (os.path.isfile(image)):
-				os.remove(image);
-			
-			# Remove nbd
-			if (os.path.isfile(nbd)):
-				os.remove(nbd);
-			
-			# Remove /var/lib/tftpboot/...
-			if (os.path.isdir(tftpboot)):
-				shutil.rmtree(tftpboot);
-			
-			# Remove .json file
-			if (os.path.isfile(json_file)):
-				os.remove(json_file);
-			
-			return {"status":True, "msg":"Image Removed"}
-		except Exception as e:
-			return {"status":False, "msg":str(e)}
-		
-		
-	#def setImage(self, image, data):
-	
-	def umount_chroot(self,chroot_dir):
-		'''
-		Umount system directories with -lazy, 
-		'''
-		ret=""
-		try:
-			# Test if exists chroot
-			if not os.path.isdir(chroot_dir):
-				print("NO DIR CHROOT: "+chroot_dir)
-				return {'status': True, 'msg':'[LmdImageManager] Directory not exists'}
-			else:
-				
-				# umount /net/mirror/llx1406
-				if (subprocess.check_output(["mount | grep "+chroot_dir+"/net/mirror | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo", "umount","-l",chroot_dir+"/net/mirror/llx1406"])
-				
-				# umount /proc
-				if (subprocess.check_output(["mount | grep "+chroot_dir+"/proc | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo", "umount","-l",chroot_dir+"/proc"])
-				
-				# umount /sys
-				if (subprocess.check_output(["mount | grep "+chroot_dir+"/sys | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo","umount","-l",chroot_dir+"/sys"])
-				
-				# umount /dev/pts
-				if (subprocess.check_output(["mount | grep "+chroot_dir+"/dev/pts | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo","umount","-l",chroot_dir+"/dev/pts"])
-				
-				# Mount /dev
-				if (subprocess.check_output(["mount | grep "+chroot_dir+"/dev | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo","umount","-l",chroot_dir+"/dev"])
-	
-				# Umount /etc
-				if (subprocess.check_output(["mount | grep "+chroot_dir+"/etc/hosts | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo","umount","-l",chroot_dir+"/etc/hosts"])
-				
-				if (subprocess.check_output(["mount | grep "+chroot_dir+"/etc/ld.so.conf.d | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo","umount","-l",chroot_dir+"/etc/ld.so.conf.d"])
-					
-				if (subprocess.check_output(["mount | grep "+chroot_dir+"/etc/nsswitch.conf | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo","umount","-l",chroot_dir+"/etc/nsswitch.conf"])
-				
-				if (subprocess.check_output(["mount | grep ' "+chroot_dir+" ' | wc -l"], shell=True))!='0\n':
-					ret=subprocess.check_output(["sudo","umount","-l",chroot_dir])
-				
-				return {'status': True, 'msg':'[LmdImageManager] All is umounted'}
-		except Exception as e:
-			return {'status': False, 'msg':'[LmdImageManager] '+str(e)}
-	#def umount_chroot(self,chroot_dir)
-	
-	def check_image_editing(self):
-		try:
-			if os.path.isfile("/tmp/.lmd-editing-chroot"):
-				return {'status':True, 'response':True}
-			else:
-				return {'status':True, 'response': False}
-		except Exception as e:
-			return {'status': False, 'msg': str(e)}
-	
-	def clean_image_editing(self):
-		try:
-			if os.path.isfile("/tmp/.lmd-editing-chroot"):
-				os.remove("/tmp/.lmd-editing-chroot");
-			
-			return {'status': True}
-		
-		except Exception as e:
-			return {'status': False, 'msg': str(e)}
-	# New method to check without vars, this method allow mount the mirror by nfs without use lliurex-mirror
-	#   check method relies into lliurex-version call, checking structure of files and dirs into /net/mirror/llx16
-	#   architectures always be all or nothing due to assumption that lliurex-mirror mirror both architectures always.
-	def check_mirror(self):
-	        try:
-	            r=subprocess.check_call(["lliurex-version","-x","mirror"])
-	            
-	            #status will be mirrorReady, msg.llx16.ARCHITECTURES(list) will always be [i386,amd64]
-	            
-	            return {'status': True,'msg': {'llx16': {'ARCHITECTURES':['i386','amd64']}}}
-	        except Exception as e:
-	            return {'status': False,'msg': {'llx16':{'ARCHITECTURES':['None']},'msg':str(e)}}
-	
-	# OLD METHOD TO CHECK WITH VARS (Disabled)
-	def check_mirror_with_vars(self):
-		try:
-			response=objects['MirrorManager'].get_all_configs();
-			
-			lliurex_mirror=objects["VariablesManager"].get_variable("LLIUREXMIRROR");
-			
-			
-			
-			if (lliurex_mirror["llx16"]["status_mirror"]!="Ok"):
-				response["status"]=False;
-				
-			return response;
-			
-		except Exception as e:
-			return {'status': False, 'msg': str(e)}
-							
-							
+        # END def getListTemplate(self, image)
+            
+                
+    def setImage(self, img_id, data):
+        '''
+        Saves metadata from *data to image
+        data is unicoded string
+        image is name
+        '''
+        
+        with self.configimagepath.joinpath(img_id+".json").open("w") as fd:
+            fd.writelines(data)
+
+        return n4d.responses.build_successful_call_response("True") 
+        
+    # def setImage(self, image, data)
+        
+        
+    def setStatusImage(self, img_id, status):
+        
+        with self.configimagepath.joinpath(img_id+".json").open("r") as fd:
+            data = json.load(fd)
+
+        data["status"] = status
+
+        return self.setImage(img_id, json.dumps(data))
+
+
+    def setNewTaskIdForImage(self, img_id, newid):
+        
+        with self.configimagepath.joinpath(img_id+".json").open("r") as fd:
+            data = json.load(fd)
+
+        data["taskid"]=newid
+        
+        return self.setImage(img_id, json.dumps(data))
+        
+        
+        
+    def deleteImage(self, img_id):
+        '''
+        N4d Method to delete an image identified by img_id
+        '''
+        
+        try:
+                        
+            # Umount anything mounted under image
+            chroot = self.chrootpath.joinpath(img_id)
+            test_chroot =  self.umount_chroot(chroot)
+            if not test_chroot["status"] :
+                return test_chroot
+            
+            # Remove chroot
+            if chroot.is_dir():
+                rmtree(chroot)
+            
+            # Removing .img
+            x = self.imagepath.join(img_id + ".img")
+            if x.is_file():
+                x.unlink()
+            
+            x = self.tftppath.joinpath(img_id)
+            # Remove /var/lib/tftpboot/...
+            if x.is_folder():
+                rmtree(x);
+           
+            x = self.configimagepath.joinpath(img_id+".json")
+            # Remove .json file
+            if x.is_file():
+                x.unlink()  
+            
+            return n4d.responses.build_successful_call_response(True)
+        
+    #def setImage(self, image, data):
+        
+    def umount_chroot(self,chroot_dir):
+        '''
+        Umount system directories with -lazy, 
+        '''
+        if not isinstance(chroot_dir,Path):
+            chroot_dir = Path(chroot_dir)
+
+        # Test if exists chroot
+        if not chroot_dir.is_dir():
+            print("NO DIR CHROOT: "+chroot_dir)
+            return n4d.responses.build_successful_call_response(True)
+        else:
+            devices_mounted = [ z[2] for z in re.findall(r"(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\n", mount().stdout.decode('utf-8')) ]
+            points_to_umount = [ "net/mirror", "proc", "sys", "dev/pts", "dev", "etc/hosts", "etc/ld.so.conf.d", "etc/nsswitch.conf" ]
+
+            for x in points_to_umount:
+                if chroot_dir.joinpath(x) in devices_mounted:
+                    umount(chroot_dir.joinpath(x),lazy=True)
+            if chroot_dir in devices_mounted:
+                umount(chroot_dir,lazy=True)
+   
+        return n4d.responses.build_successful_call_response(True)
+
+    #def umount_chroot(self,chroot_dir)
+        
+    def check_image_editing(self):
+        if self.lmd_editing_path.is_file():
+            return n4d.responses.build_successful_call_response(True)
+        else:
+            return n4d.responses.build_successful_call_response(False) 
+    
+    def clean_image_editing(self):
+        if self.lmd_editing_path.is_file():
+            self.lmd_editing_path.unlink()
+
+        return n4d.responses.build_successful_call_response(True)
+
+    def check_mirror(self):
+        '''
+            New method to check without vars, this method allow mount the mirror by nfs without use lliurex-mirror
+            check method relies into lliurex-version call, checking structure of files and dirs into /net/mirror/llx16
+            architectures always be all or nothing due to assumption that lliurex-mirror mirror both architectures always.
+        '''
+        try:
+            lliurex_version(x="mirror")
+            return n4d.responses.build_successful_call_response({"llx21":{"ARCHITECTURES":["i386","amd64"]}})
+        except sh.ErrorReturnCode_1:
+            return n4d.responses.build_failed_call_response(LmdImageManager.MIRROR_NOT_EXISTS)
+    
+    # OLD METHOD TO CHECK WITH VARS (Disabled)
+    def check_mirror_with_vars(self):
+        
+        lliurex_mirror = self.core.get_variable("LLIUREXMIRROR")
+        
+        if lliurex_mirror["llx21"]["status_mirror"] != "Ok":
+            return n4d.responses.build_failed_call_response(MIRROR_NOT_EXISTS)
+
+        mirror_manager = self.core.get_plugin("MirrorManager")
+        return mirror_manager.get_all_configs()
--- lmd-server.install/usr/share/n4d/python-plugins/LmdTemplateManager.py
+++ lmd-server.install/usr/share/n4d/python-plugins/LmdTemplateManager.py
@@ -1,162 +1,128 @@
+from pathlib import Path
+from io import StringIO
 import json
-import StringIO
-import os
-import os.path
-import ConfigParser
+from configparser import ConfigParse
+
+import json
 import shlex
 import urllib2 
 
 class LmdTemplateManager:
-	
-		
-	def __init__(self):
-		self.templatepath="/etc/ltsp/templates"
+   
+    CONFIG_FAILED = -40
+    FILE_NOT_EXISTS = -50
+   
 
-		
-		pass
-	#def __init__
-	
-	
-	def testMirror(self, mirror, dist):
-		'''
-		Tests if file Release exists inside mirror in template
-		'''
-		#template=self.getTemplate(templatefile)
-		
-		#json_template=json.loads(template)['default']
-		#mirror=json_template['mirror']
-		#dist=json_template['dist']
-		
-		release_file=str(mirror+"/dists/"+dist+"/Release")
-		
-		if release_file[0:4]=='file':
-			if os.path.isfile(release_file[7:]):
-				return True
-			else:
-				return False
-		else:
-			try:
-				content = urllib2.urlopen(release_file).read()
-			except Exception as e:
-				return False
+    def __init__(self):
+        self.templatepath = Path("/etc/ltsp/templates")
 
-			return True
-	
-	
-	def getTemplateList(self):
-		'''
-		Reads the file list of templates from /etc/ltsp/templates
-		Returna a JSON List.
-		'''
-		templatelist=[]
-		
-		for i in os.listdir(self.templatepath):
-			if '.conf' in i:
-				templatelist.append(str(i))
-				
-		return json.dumps(templatelist)
-			
-			
-	# END def GetListTemplates(self)
+    #def __init__
+        
+        
+    def testMirror(self, mirror, dist):
+        '''
+        Tests if file Release exists inside mirror in template
+        '''
+        
+        release_file=str(mirror+"/dists/"+dist+"/Release")
+        result = True 
+        if release_file[0:4]=='file':
+            if not os.path.isfile(release_file[7:]):
+                result = False
+        else:
+            try:
+                content = urllib2.urlopen(release_file).read()
+            except Exception as e:
+                result = False
 
-	def deleteTemplate(self, filename):
-		try:
-			fullfilename=os.path.join("/etc/ltsp/templates", filename)
-			print("Deleting: ", fullfilename)
-			if(os.path.exists(fullfilename)):
-				print("exists")
-				os.remove(fullfilename)
-				return True
-			return False
-		except Exception as e:
-			return False
-		
+        return n4d.responses.build_successful_call_response(result)
+    
+    
+    def getTemplateList(self):
+        '''
+        Reads the file list of templates from /etc/ltsp/templates
+        Returna a JSON List.
+        '''
+        templatelist=[]
+        
+        for i in self.templatepath.iterdir():
+            if i.suffix == ".conf" :
+                templatelist.append(str(i))
+                    
+        return n4d.responses.build_successful_call_response(json.dumps(templatelist))
+                
+                    
+        # END def GetListTemplates(self)
 
+    def deleteTemplate(self, filename):
+        result = False
+        if self.templatepath.joinpath(filename).exists():
+            self.templatepath.joinpath(filename).unlink()
+            result = True
+        return n4d.responses.build_successful_call_response(result)
 
-	def getTemplate(self, template):
-		'''
-		Reads the file template from /etc/ltsp/templates
-		Returna a JSON string with the config options
-		'''
-		try:
-			#template=self.getTemplate(templatefile)
-		
-		#json_template=json.loads(template)['default']
-		#mirror=json_template['mirror']
-		#dist=json_template['dist']
-			
-			
-			config = StringIO.StringIO()
-			config.write('[meta_inf]\n')
-			config.write('name="'+template+'"\n')
-			
-			# Check if mirror is available
-			#available=self.testMirrorTemplate(template)
-			#config.write('available="'+str(available)+'"\n')
-			
-			
-			config.write('[default]\n')
-			config.write(open(str(self.templatepath)+"/"+str(template)).read())
-			config.seek(0, os.SEEK_SET)
-			cp = ConfigParser.ConfigParser()
-			cp.readfp(config)
-			aux = cp._sections
-			for x in list(aux.keys()):
-				for y in aux[x]:
-					if len(aux[x][y])>0 and aux[x][y][0] == '"' and aux[x][y][-1] == '"':
-						aux[x][y] = aux[x][y][1:-1]
+    def checkTemplateExists(self, template):
+        return n4d.responses.build_successful_call_response(self.templatepath.joinpath(template).exists()
 
-			# Checking Mirror
-			json_template=aux['default']
-			mirror=json_template['mirror']
-			dist=json_template['dist']
-			print("Mirror is"+mirror)
-			print("Dist is: "+dist)			
-			aux["meta_inf"]["available"]=self.testMirror(mirror, dist)
+    def setTemplate(self, template, config):
+        '''
+        Writes in /etc/ltsp/templates the config file "template" with
+        the "config" (JSON format) content.
+        '''
+        with self.templatepath.joinpath(template).open("w") as fd:
+            fd.write("# LMD Customuzation file for LTSP\n\n")
 
-			return json.dumps(aux,encoding="utf-8",ensure_ascii=False)
-			#return json.dumps(cp.items('default'));
-			
-			
-		except Exception as e:
-			return "Exception: "+str(e)
-		# END def getListTemplate(self, template)
-		
-		
-	def checkTemplateExists(self, template):
-		if(os.path.isfile(str(self.templatepath)+"/"+str(template))):
-			return "True"
-		else:
-			return "False"
-		
-	
-	def setTemplate(self, template, config):
-		'''
-		Writes in /etc/ltsp/templates the config file "template" with
-		the "config" (JSON format) content.
-		'''
-		try:
-			print("config is:", config);
-			print(type(config))
-			f = open(str(self.templatepath)+"/"+str(template), 'w')
-			f.write("# LMD Customuzation file for LTSP\n\n")
-			
-			conf=json.loads(config)
-						
-			print(conf['default'])
-			print("is...",type(conf['default']))
-			print(len(conf['default']))
-			i=0
-			for (key, value) in list(conf['default'].items()):
-				if (key.upper()=="__NAME__"):
-					continue
-				# Removing " from value and ignore comments
-				f.write("{0}=\"{1}\"\n".format(key.upper(), value.replace('"', '').split("#")[0]))
-			f.close()
-			return True
-		except Exception as e:
-			
-			print("Exception: "+str(e))
-			return "Exception: "+str(e)
-	
-	# def setTemplate(self, template, config)
+            conf = json.loads(config)
+                                
+            for (key, value) in list(conf['default'].items()):
+                if (key.upper()=="__NAME__"):
+                    continue
+            # Removing " from value and ignore comments
+            fd.write("{0}=\"{1}\"\n".format(key.upper(), value.replace('"', '').split("#")[0]))
+        return n4d.responses.build_successful_call_response(True)
+
+    # def setTemplate(self, template, config)
+
+    def getTemplate(self, template):
+        '''
+        Reads the file template from /etc/ltsp/templates
+        Returna a JSON string with the config options
+        '''
+
+        if not self.templatepath.joinpath(template).exists():
+            return n4d.responses.build_failed_call_response(LmdTemplateManager.FILE_NOT_EXISTS)
+
+        config = StringIO.StringIO()
+        config.write('[meta_inf]\n')
+        config.write('name="'+template+'"\n')
+        config.write('[default]\n')
+        
+        with self.templatepath.joinpath(template).open("r") as fd:
+            config.write(fd.read())
+        config.seek(0, os.SEEK_SET)
+        cp = ConfigParser()
+
+        try:
+            cp.readfp(config)
+        except Exception as e:
+            return n4d.responses.build_failed_call_response(LmdTemplateManager.CONFIG_FAILED)
+        
+        aux = cp._sections
+        for x in list(aux.keys()):
+            for y in aux[x]:
+                if len(aux[x][y])>0 and aux[x][y][0] == '"' and aux[x][y][-1] == '"':
+                    aux[x][y] = aux[x][y][1:-1]
+
+        # Checking Mirror
+
+        json_template = aux['default']
+        mirror = json_template['mirror']
+        dist = json_template['dist']
+        result = self.testMirror(mirror, dist)
+        if result['status'] == n4d.responses.CALL_SUCCESSFUL :
+            aux["meta_inf"]["available"] = result["result"]
+
+        return n4d.responses.build_successful_call_response(json.dumps(aux,encoding="utf-8",ensure_ascii=False))
+        
+        # END def getListTemplate(self, template)
+        
